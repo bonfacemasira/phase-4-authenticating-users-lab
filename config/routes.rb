234d4e0a24c6f80ea423e1b881892ae2265a3520
Bonface Masira <bonfacemasira@gmail.com>
2022-10-21 00:08:02 +0300

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
+
+  #authentication
+  post "/login", to: "sessions#create"
+
+  #staying logged in
+  get "/me", to: "users#show"
+
+  #logging out
+  delete "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
